047739a87ad1d00409cf96fbac9686d6ab6931b5
JosephAbbey <joseph@abbey1.org.uk>
2022-02-28 14:17:52 +0000

    stuf

--- static/item.mjs
+++ static/item.mjs
@@ -11,6 +11,7 @@ export default class CatalogueItem extends HTMLElement {
             "found-in": data.location,
             title: data.title,
             category: data.category,
+            id: this.attributes.id.value,
             image: this.attributes.id.value
         }
     
@@ -18,30 +19,34 @@ export default class CatalogueItem extends HTMLElement {
         const _template = document.createElement('template');
     
         _style.innerHTML = `
-        img {
-          width: 6rem;
-          height: 6rem;
-          display: inline-block;
-        }
-        div {
-          display: grid;
-          grid-template-columns: 6.5rem 12rem;
-          padding: 1rem;
-          margin: 0.5rem;
-          border-radius: 1rem;
-          border: 1px solid #000;
-          width: fit-content;
-        }
+          img {
+            width: 6rem;
+            height: 6rem;
+            display: inline-block;
+            border-radius: 0.625rem;
+          }
+          div {
+            display: grid;
+            grid-template-columns: 6.5rem 12rem;
+            padding: 1rem;
+            margin: 0.5rem;
+            border-radius: 1rem;
+            border: 1px solid #000;
+            width: fit-content;
+            cursor: pointer;
+          }
         `;
     
         _template.innerHTML = `
-        <div>
-          <img title="${this.attrs.title}" src="${config.PHOTO_API + this.attrs.image}" />
-          <h1>
-            ${this.attrs.title}
-          </h1>
-        </div>
-      `;
+          <div>
+            <img title="${this.attrs.title}" src="${config.PHOTO_API + this.attrs.image}" />
+            <h1>
+              ${this.attrs.title}
+            </h1>
+          </div>
+        `;
+
+        this.onclick = () => window.location = `/item?id=${this.attrs.id}`;
     
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.appendChild(_style);
