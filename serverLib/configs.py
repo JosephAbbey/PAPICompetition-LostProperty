55f112100164f518de406e781aa2b1730536bc09
Benjamin Jones <saganashdev@gmail.com>
2022-03-01 14:04:09 +0000

    adminAuth

--- serverLib/configs.py
+++ serverLib/configs.py
@@ -7,8 +7,9 @@ import datetime
 MAX_STORE: int = 5 # Number of store boxes
 PAGE_SIZE: int = 10 # Items per catalogue page
 
-DATABASE: str = "lostProperty.db" # Database file
-NOTIFY: str = "notifSave.json" # Savefile (JSON)
+DATA_FOLDER: str = "data"
+DATABASE: str = f"{DATA_FOLDER}/lostProperty.db" # Database file
+NOTIFY: str = f"{DATA_FOLDER}/notifSave.json" # Savefile (JSON)
 
 EXPIRY_TIME: datetime.timedelta = datetime.timedelta(hours=3) # Time until item expires
 NOTIFY_TIME: datetime.timedelta = datetime.timedelta(weeks=3) # How often to notify
